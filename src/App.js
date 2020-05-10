57b96fbcd5af8a1552f9158ad2564d2e12d0253d
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-10 15:56:52 -0500

    Creado el componente de formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Form from './components/Form';
 
 function App() {
   return (
+    <>
     <h1>Administrador de Pacientes</h1>
+    <div className="container">
+      <div className="row">
+        <div className="one-half column">
+          <Form />
+        </div>
+        <div className="one-half column">
+          2
+        </div>
+      </div>
+    </div>
+    </>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <>
+        <h1>Desde Formulario</h1>
+        </>
+    );
+};
+
+export default Form;
