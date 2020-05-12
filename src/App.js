1270fb5efbeaf79d495b6109a53ada5d1cf2c96d
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-11 22:16:05 -0500

    Preguntando al usuario por su presupuesto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Question from './components/Question';
 
 function App() {
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+          <Question />
+        </div>
       </header>
     </div>
   );
