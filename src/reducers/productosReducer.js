3117be335a5e986e96d4d85b059336cbc6d464ad
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-17 23:46:35 -0500

    Definiendo las primeras acciones de Redux

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,3 +1,10 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from '../types';
+
+
 // Cada reducer tiene su propio state
 const initialState = {
     productos: [],
