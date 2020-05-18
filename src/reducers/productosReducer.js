c30980ee0698da178631c22d95579915c89fd314
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-18 01:07:12 -0500

    Agregando un nuevo producto al State

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -14,6 +14,20 @@ const initialState = {
 
 export default function ( state = initialState, action ) {
     switch( action.type ) {
+
+        case AGREGAR_PRODUCTO: 
+            return { // imagina este return como un setState
+                ...state,
+                loading: action.payload
+            }
+
+        case AGREGAR_PRODUCTO_EXITO: 
+            return {
+                ...state,
+                loading: false,
+                productos: [ ...state.productos, action.payload ]
+            }
+
         default:
             return state;
     }
