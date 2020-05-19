84d0fae9c6cb4d48df8cc342b40d7333d66a98b8
Stiven Trujillo <strujillo1293@gmail.com>
2020-05-19 00:09:07 -0500

    Insertando Productos en la API

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -3,15 +3,22 @@ import {
     AGREGAR_PRODUCTO_EXITO,
     AGREGAR_PRODUCTO_ERROR
 } from '../types';
+import clienteAxios from '../config/axios';
 
 // Crear nuevos productos
 export function crearNuevoProductoAction( producto ) {
-    return ( dispatch ) => {
+    return async ( dispatch ) => {
         dispatch( agregarProducto() );
 
         try {
+            // Insertar en la API
+            await clienteAxios.post( '/hola', producto );
+
+            // Sí todo sale bien, actualizar el State
             dispatch( agregarProductoExito( producto ) );
         } catch ( error ) {
+            console.log( error );
+            // Sí hay un error, cambiar el State
             dispatch( agregarProductoError( true ) );
         }
     }
