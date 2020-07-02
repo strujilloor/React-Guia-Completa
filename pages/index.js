7d1d44fc6a606ee832066f8e0e8902b7e7251e37
Stiven Trujillo Ortiz <strujillo1293@gmail.com>
2020-07-02 00:38:34 -0500

    CSS en Next.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div>
       <h1>Inicio</h1>
+
+      {/* CSS dentro de JS */}
+      <style jsx>{`
+          h1 {
+            color: red;
+          }
+      `}</style>
     </div>
   )
 }
