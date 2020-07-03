9d282e65d4dc2d45ebf2bcadc9932003a7df1dea
Stiven Trujillo Ortiz <strujillo1293@gmail.com>
2020-07-03 01:41:06 -0500

    Header con buscador, agregado al layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Link from 'next/link'
+import Header from './Header';
 
 const Layout = ({ children  }) => {
 
     return (
         <>
-            <h1>Header</h1>
-
-            <nav>
-                <Link href="/">Inicio</Link>
-                <Link href="/nosotros">Nosotros</Link>
-            </nav>
+            <Header/>
+            
             <main>
                 { children }
             </main>
---
+++ components/ui/Buscar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Buscar = () => {
+    return ( 
+        <form>
+            <input type="text"/>
+
+            <button type="submit">Buscar</button>
+        </form>
+    );
+}
+
+export default Buscar; 
+
+// ui : user interface
